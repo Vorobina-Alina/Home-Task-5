5cf42a4e73d7c47e20e5c62c6201c62a1175a456
Vorobina-Alina <151908614+Vorobina-Alina@users.noreply.github.com>
2023-11-29 15:57:52 +0300

    init

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'index.html'),
-      filename: 'index.html',
-    }),
-  ],
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html'),
+            filename: 'index.html',
+        }),
+    ],
+    devServer: {
+        watchFiles: path.join(__dirname, 'src'),
+        port: 9000,
+    },
 };
